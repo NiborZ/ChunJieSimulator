4fc23b430d1b61d8d78ec17a05b94c63c6cbf933
VaanHUANG <axis120151@gmail.com>
2019-01-27 10:50:18 +0800

    泡麵+主菜單+失敗畫面

---
+++ 泡面/Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+using System.Collections.Generic;       //Allows us to use Lists. 
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public int money = 8888;
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+       
+    }
+
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+       
+
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+        
+    }
+
+    //GameOver is called when the player reaches 0 food points
+    public void GameOver()
+    {
+
+    }
+
+}
